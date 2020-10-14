7953954c7a4e825af32c19a5dc23efac952e5200
Enes Bahadır YILDIRIM <enesbahadiryildirim@gmail.com>
2020-10-14 15:31:45 +0300

    DiscountService sınıfı eklendi, DiscountController sınıfı ile entegrasyonu yapıldı.

--- src/main/java/com/preschool/controller/DiscountController.java
+++ src/main/java/com/preschool/controller/DiscountController.java
@@ -1,14 +1,12 @@
 package com.preschool.controller;
 
 import com.preschool.model.Discount;
-import com.preschool.repository.DiscountRepository;
-import com.preschool.exeption.PreschoolNotFoundExection;
+import com.preschool.service.DiscountService;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -16,62 +14,75 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 public class DiscountController {
 
-    private final DiscountRepository discountRepository;
+    private final DiscountService discountService;
 
-    public DiscountController(DiscountRepository discountRepository) {
-        this.discountRepository = discountRepository;
+    public DiscountController(DiscountService discountService) {
+        this.discountService = discountService;
     }
 
+    /**
+     *İndirim listesine erişmek için çağrılan GET metodu
+     * @return İndirim listesini HATEOAS uygun olarak döner
+     */
     @GetMapping("/discounts")
-    CollectionModel<EntityModel<Discount>> all()
+    public CollectionModel<EntityModel<Discount>> getDiscounts()
     {
-        List<EntityModel<Discount>> discounts = discountRepository.findAll().stream()
-                .map(discount -> EntityModel.of(discount,
-                        linkTo(methodOn(DiscountController.class).one(discount.getId())).withSelfRel(),
-                        linkTo(methodOn(DiscountController.class).all()).withRel("discounts")))
-                .collect(Collectors.toList());
-
-        return CollectionModel.of(discounts, linkTo(methodOn(DiscountController.class).all()).withSelfRel());
+        List<EntityModel<Discount>> discounts = discountService.getDiscounts();
+        return CollectionModel.of(discounts, linkTo(methodOn(DiscountController.class)
+                .getDiscounts()).withSelfRel());
     }
 
+    /**
+     *İstenen indirimi id üzerinden arayarak ilgili anaokulunun dönen metod
+     * @param id ilgili indirimin id'si
+     * @return HATEOAS uygun olarak ilgili indirim nesnesini döner
+     */
     @GetMapping("/discounts/{id}")
-    EntityModel<Discount> one(@PathVariable Integer id)
+    public EntityModel<Discount> getDiscountById(@PathVariable Integer id)
     {
-        Discount discount = discountRepository.findById(id)
-                .orElseThrow(() -> new PreschoolNotFoundExection(id));
+        Discount discount = discountService.getDiscountById(id);
 
         return EntityModel.of(discount,
-                linkTo(methodOn(DiscountController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(DiscountController.class).all()).withRel("discounts"));
+                linkTo(methodOn(DiscountController.class).getDiscountById(id)).withSelfRel(),
+                linkTo(methodOn(DiscountController.class).getDiscounts()).withRel("discounts"));
     }
 
+    /**
+     *İndirim oluşturmak için çağrılan POST metodu
+     * @param newDiscount yeni oluşturulan olan indirim nesnesi
+     * @return HATEOAS uygun olarak ilgili indirim nesnesini döner
+     */
     @PostMapping("/discounts")
-    Discount newDiscount(@RequestBody Discount newDiscount)
+    public EntityModel<Discount> createDiscount(@RequestBody Discount newDiscount)
     {
-        return discountRepository.save(newDiscount);
+        discountService.createDiscount(newDiscount);
+        return EntityModel.of(newDiscount,
+                linkTo(methodOn(DiscountController.class).getDiscountById(newDiscount.getId())).withSelfRel(),
+                linkTo(methodOn(DiscountController.class).getDiscounts()).withRel("discounts"));
     }
 
+    /**
+     * İndirim silmek için çağırılan DELETE metodu
+     * @param id silinecek olan indirimin id'si
+     */
     @DeleteMapping("/discounts/{id}")
-    void deleteDiscount(@PathVariable Integer id) {
-        discountRepository.deleteById(id);
+    public void deleteDiscount(@PathVariable Integer id) {
+        discountService.deleteDiscount(id);
     }
 
+    /**
+     * İndirim düzenlemek için çağırılan PUT metodu
+     * @param newDiscount değişiklik yapılmış olan indirim nesnesi
+     * @param id değiştirilecek olan indirimin id'si
+     * @return HATEOAS uygun olarak düzenlenen yeni indirimi döner
+     */
     @PutMapping("/discounts/{id}")
-    Discount replaceDiscount(@RequestBody Discount newDiscount, @PathVariable Integer id)
+    public EntityModel<Discount> updateDiscount(@RequestBody Discount newDiscount, @PathVariable Integer id)
     {
-        return discountRepository.findById(id)
-                .map(discount -> {
-                    discount.setDiscountName(newDiscount.getDiscountName());
-                    discount.setDiscountType(newDiscount.getDiscountType());
-                    discount.setOrganizationName(newDiscount.getOrganizationName());
-                    //discount.setPreschoolNamesAndTheirDiscounts(newDiscount.getPreschoolNamesAndTheirDiscounts());
-                    discount.setTypeOfUser(newDiscount.getTypeOfUser());
-                    return discountRepository.save(discount);
-                })
-                .orElseGet(() -> {
-                    newDiscount.setId(id);
-                    return discountRepository.save(newDiscount);
-                });
+        Discount discount = discountService.updateDiscount(newDiscount, id);
+        return EntityModel.of(discount,
+                linkTo(methodOn(DiscountController.class).getDiscountById(id)).withSelfRel(),
+                linkTo(methodOn(DiscountController.class).getDiscounts()).withRel("discounts"));
     }
 
 }
---
+++ src/main/java/com/preschool/service/DiscountService.java
@@ -0,0 +1,89 @@
+package com.preschool.service;
+
+import com.preschool.controller.DiscountController;
+import com.preschool.exeption.PreschoolNotFoundExection;
+import com.preschool.model.Discount;
+import com.preschool.repository.DiscountRepository;
+import org.springframework.hateoas.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class DiscountService {
+
+    private final DiscountRepository discountRepository;
+
+    public DiscountService(DiscountRepository discountRepository) {
+        this.discountRepository = discountRepository;
+    }
+
+    /**
+     * indirim listesi oluşturma metodu, ilgili listeyi repository'den oluşturur
+     * @return HATEOAS uygun olarak indirim listesini döner
+     */
+    public List<EntityModel<Discount>> getDiscounts()
+    {
+        return discountRepository.findAll().stream()
+                .map(discount -> EntityModel.of(discount,
+                        linkTo(methodOn(DiscountController.class).getDiscountById(discount.getId())).withSelfRel(),
+                        linkTo(methodOn(DiscountController.class).getDiscounts()).withRel("discounts")))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * İstenen indirimi verilen id'ye göre getiren metod
+     * @param id ilgili indirimin id'si
+     * @return ilgili indirim nesnesi
+     */
+    public Discount getDiscountById(int id)
+    {
+        return discountRepository.findById(id)
+                .orElseThrow(() -> new PreschoolNotFoundExection(id));
+    }
+
+    /**
+     * İndirim oluşturma metodu, ilgili anaokulunu repository'e ekler
+     * @param newDiscount Oluşturulacak olan İndirim
+     */
+    public void createDiscount(Discount newDiscount)
+    {
+        discountRepository.save(newDiscount);
+    }
+
+    /**
+     * Silinmek istenen indirimi verilen id'ye göre silen metod
+     * @param id ilgili indirimin id'si
+     */
+    public void deleteDiscount(int id)
+    {
+        discountRepository.deleteById(id);
+    }
+
+    /**
+     * Düzenlenmek istenen indirimi, verilen id ve yeni indirim nesnesine göre düzenleyen metod
+     * @param newDiscount değişiklik yapılmış indirim nesnesi
+     * @param id değişiklik yapılmış olan indirimin id'si
+     * @return değişiklik yapılmış olan indirim nesnesini döner
+     */
+    public Discount updateDiscount (Discount newDiscount, int id)
+    {
+        return discountRepository.findById(id)
+                .map(discount -> {
+                    discount.setDiscountName(newDiscount.getDiscountName());
+                    discount.setDiscountType(newDiscount.getDiscountType());
+                    discount.setOrganizationName(newDiscount.getOrganizationName());
+                    //discount.setPreschoolNamesAndTheirDiscounts(newDiscount.getPreschoolNamesAndTheirDiscounts());
+                    discount.setUserType(newDiscount.getUserType());
+                    return discountRepository.save(discount);
+                })
+                .orElseGet(() -> {
+                    newDiscount.setId(id);
+                    return discountRepository.save(newDiscount);
+                });
+    }
+}
